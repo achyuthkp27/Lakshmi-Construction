2fc85db4bc46de63d804849749233c0a3459a89e
Achyuth KP <kpachyuthz@gmail.com>
2022-04-13 09:01:40 +0530

    Made Chnages in Website

--- sw.js
+++ sw.js
@@ -39,42 +39,61 @@
 //     return cached;
 //   }
 // }
-var cacheAll = false;
-var CACHE_NAME = "webapk-cache";
-var urlsToCache = ["./", "./index.html", "./assets"];
-var urlsNotToCache = [];
 
-// Install Event
-self.addEventListener("install", function (event) {
-  console.log("[SW] install event: ", event);
-  // Perform install steps
+const PRECACHE = "cache-v3";
+const RUNTIME = "runtime-1";
+
+const PRECACHE_URLS = ["index.html", "./", "./assets"];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("[SW] Opened cache: ", cache);
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
-// Fetch Event
-self.addEventListener("fetch", function (event) {
-  console.log("[SW] fetch event: ", event);
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) return response;
-      else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1)
-        return fetch(event.request);
-      else {
-        fetch(event.request).then(function (response) {
-          if (!response || response.status !== 200 || response.type !== "basic")
-            return response;
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        });
-      }
-    })
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (
+    event.request.url.startsWith(self.location.origin) &&
+    self.location.hostname !== "localhost"
+  ) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
