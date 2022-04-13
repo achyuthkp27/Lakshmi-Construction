6ba216af3857f9a18a97b7d93b97b671a1b412f7
Achyuth KP <kpachyuthz@gmail.com>
2022-04-13 09:18:48 +0530

    Made Chnages in Website

--- sw.js
+++ sw.js
@@ -1,99 +1,41 @@
-// const cacheName = "LakshmiConstruction-v1";
-// const staticAssets = ["./", "./index.html", "./assets"];
+const cacheName = "LakshmiConstruction-v1";
+const staticAssets = ["./", "./index.html", "./assets"];
 
-// self.addEventListener("install", async (e) => {
-//   const cache = await caches.open(cacheName);
-//   await cache.addAll(staticAssets);
-//   return self.skipWaiting();
-// });
-
-// self.addEventListener("activate", (e) => {
-//   self.clients.claim();
-// });
-
-// self.addEventListener("fetch", async (e) => {
-//   const req = e.request;
-//   const url = new URL(req.url);
-
-//   if (url.origin === location.origin) {
-//     e.respondWith(cacheFirst(req));
-//   } else {
-//     e.respondWith(networkAndCache(req));
-//   }
-// });
-
-// async function cacheFirst(req) {
-//   const cache = await caches.open(cacheName);
-//   const cached = await cache.match(req);
-//   return cached || fetch(req);
-// }
-
-// async function networkAndCache(req) {
-//   const cache = await caches.open(cacheName);
-//   try {
-//     const fresh = await fetch(req);
-//     await cache.put(req, fresh.clone());
-//     return fresh;
-//   } catch (e) {
-//     const cached = await cache.match(req);
-//     return cached;
-//   }
-// }
-
-const PRECACHE = "cache-v3";
-const RUNTIME = "runtime-1";
-
-const PRECACHE_URLS = ["./index.html", "./", "./assets"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (
-    event.request.url.startsWith(self.location.origin) &&
-    self.location.hostname !== "localhost"
-  ) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
   }
 });
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
