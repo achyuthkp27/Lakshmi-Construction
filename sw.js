992755f10193efab93e73acfb3f1c25d33cc5bbd
Achyuth KP <kpachyuthz@gmail.com>
2022-04-13 08:46:09 +0530

    Made Chnages in Website

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = "news-v1";
-const staticAssets = ["./", "./index.html", "./assets/js/main.js"];
+const staticAssets = ["./", "./index.html", "./assets"];
 
 self.addEventListener("install", async (e) => {
   const cache = await caches.open(cacheName);
