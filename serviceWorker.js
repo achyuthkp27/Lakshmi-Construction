d03eae23320258156b6b279e281ae825f2265d1e
Achyuth KP <kpachyuthz@gmail.com>
2022-04-14 08:17:49 +0530

    Made Chnages in Website

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,46 +1,33 @@
 //Cache polyfil to support cacheAPI in all browsers
-importScripts('./cache-polyfill.js');
+importScripts("./cache-polyfill.js");
 
-var cacheName = 'cache-v4';
+var cacheName = "cache-v4";
 
 //Files to save in cache
 var files = [
-  './',
-  './index.html?utm=homescreen', //SW treats query string as new request
-  'https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700', //caching 3rd party content
-  './css/styles.css',
-  './images/icons/android-chrome-192x192.png',
-  './images/push-on.png',
-  './images/push-off.png',
-  './images/icons/favicon-16x16.png',
-  './images/icons/favicon-32x32.png',
-  './js/main.js',
-  './js/app.js',
-  './js/offline.js',
-  './js/push.js',
-  './js/sync.js',
-  './js/toast.js',
-  './js/share.js',
-  './js/menu.js',
-  './manifest.json'
+  "./",
+  "./index.html?utm=homescreen", //SW treats query string as new request
+  "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i", //caching 3rd party content
+  "./assets",
+  "./manifest.json",
 ];
 
 //Adding `install` event listener
-self.addEventListener('install', (event) => {
-  console.info('Event: Install');
+self.addEventListener("install", (event) => {
+  console.info("Event: Install");
 
   event.waitUntil(
-    caches.open(cacheName)
-    .then((cache) => {
+    caches.open(cacheName).then((cache) => {
       //[] of files to cache & if any of the file not present `addAll` will fail
-      return cache.addAll(files)
-      .then(() => {
-        console.info('All files are cached');
-        return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
-      })
-      .catch((error) =>  {
-        console.error('Failed to cache', error);
-      })
+      return cache
+        .addAll(files)
+        .then(() => {
+          console.info("All files are cached");
+          return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+        })
+        .catch((error) => {
+          console.error("Failed to cache", error);
+        });
     })
   );
 });
@@ -50,8 +37,8 @@ self.addEventListener('install', (event) => {
 */
 
 //Adding `fetch` event listener
-self.addEventListener('fetch', (event) => {
-  console.info('Event: Fetch');
+self.addEventListener("fetch", (event) => {
+  console.info("Event: Fetch");
 
   var request = event.request;
   var url = new URL(request.url);
@@ -81,7 +68,7 @@ async function networkFirst(request) {
     const networkResponse = await fetch(request);
     // Cache the dynamic API response
     dynamicCache.put(request, networkResponse.clone()).catch((err) => {
-      console.warn(request.url + ': ' + err.message);
+      console.warn(request.url + ": " + err.message);
     });
     return networkResponse;
   } catch (err) {
@@ -95,8 +82,8 @@ async function networkFirst(request) {
 */
 
 //Adding `activate` event listener
-self.addEventListener('activate', (event) => {
-  console.info('Event: Activate');
+self.addEventListener("activate", (event) => {
+  console.info("Event: Activate");
 
   //Navigation preload is help us make parallel request while service worker is booting up.
   //Enable - chrome://flags/#enable-service-worker-navigation-preload
@@ -104,30 +91,32 @@ self.addEventListener('activate', (event) => {
   //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
 
   // Check if navigationPreload is supported or not
-  // if (self.registration.navigationPreload) { 
+  // if (self.registration.navigationPreload) {
   //   self.registration.navigationPreload.enable();
   // }
-  // else if (!self.registration.navigationPreload) { 
+  // else if (!self.registration.navigationPreload) {
   //   console.info('Your browser does not support navigation preload.');
   // }
 
   //Remove old and unwanted caches
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            return caches.delete(cache); //Deleting the old cache (cache v1)
-          }
-        })
-      );
-    })
-    .then(function () {
-      console.info("Old caches are cleared!");
-      // To tell the service worker to activate current one 
-      // instead of waiting for the old one to finish.
-      return self.clients.claim(); 
-    }) 
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== cacheName) {
+              return caches.delete(cache); //Deleting the old cache (cache v1)
+            }
+          })
+        );
+      })
+      .then(function () {
+        console.info("Old caches are cleared!");
+        // To tell the service worker to activate current one
+        // instead of waiting for the old one to finish.
+        return self.clients.claim();
+      })
   );
 });
 
@@ -136,20 +125,20 @@ self.addEventListener('activate', (event) => {
 */
 
 //Adding `push` event listener
-self.addEventListener('push', (event) => {
-  console.info('Event: Push');
+self.addEventListener("push", (event) => {
+  console.info("Event: Push");
 
-  var title = 'Push notification demo';
+  var title = "Push notification demo";
   var body = {
-    'body': 'click to return to application',
-    'tag': 'demo',
-    'icon': './images/icons/apple-touch-icon.png',
-    'badge': './images/icons/apple-touch-icon.png',
+    body: "click to return to application",
+    tag: "demo",
+    icon: "./images/icons/apple-touch-icon.png",
+    badge: "./images/icons/apple-touch-icon.png",
     //Custom actions buttons
-    'actions': [
-      { 'action': 'yes', 'title': 'I ♥ this app!'},
-      { 'action': 'no', 'title': 'I don\'t like this app'}
-    ]
+    actions: [
+      { action: "yes", title: "I ♥ this app!" },
+      { action: "no", title: "I don't like this app" },
+    ],
   };
 
   event.waitUntil(self.registration.showNotification(title, body));
@@ -161,21 +150,23 @@ self.addEventListener('push', (event) => {
   another sync is scheduled to retry (will will also waits for network connection)
 */
 
-self.addEventListener('sync', (event) => {
-  console.info('Event: Sync');
+self.addEventListener("sync", (event) => {
+  console.info("Event: Sync");
 
   //Check registered sync name or emulated sync from devTools
-  if (event.tag === 'github' || event.tag === 'test-tag-from-devtools') {
+  if (event.tag === "github" || event.tag === "test-tag-from-devtools") {
     event.waitUntil(
       //To check all opened tabs and send postMessage to those tabs
-      self.clients.matchAll().then((all) => {
-        return all.map((client) => {
-          return client.postMessage('online'); //To make fetch request, check app.js - line no: 122
+      self.clients
+        .matchAll()
+        .then((all) => {
+          return all.map((client) => {
+            return client.postMessage("online"); //To make fetch request, check app.js - line no: 122
+          });
         })
-      })
-      .catch((error) => {
-        console.error(error);
-      })
+        .catch((error) => {
+          console.error(error);
+        })
     );
   }
 });
@@ -185,40 +176,40 @@ self.addEventListener('sync', (event) => {
 */
 
 //Adding `notification` click event listener
-self.addEventListener('notificationclick', (event) => {
-  var url = 'https://demopwa.in/';
+self.addEventListener("notificationclick", (event) => {
+  var url = "https://demopwa.in/";
 
   //Listen to custom action buttons in push notification
-  if (event.action === 'yes') {
-    console.log('I ♥ this app!');
-  }
-  else if (event.action === 'no') {
-    console.warn('I don\'t like this app');
+  if (event.action === "yes") {
+    console.log("I ♥ this app!");
+  } else if (event.action === "no") {
+    console.warn("I don't like this app");
   }
 
   event.notification.close(); //Close the notification
 
   //To open the app after clicking notification
   event.waitUntil(
-    clients.matchAll({
-      type: 'window'
-    })
-    .then((clients) => {
-      for (var i = 0; i < clients.length; i++) {
-        var client = clients[i];
-        //If site is opened, focus to the site
-        if (client.url === url && 'focus' in client) {
-          return client.focus();
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clients) => {
+        for (var i = 0; i < clients.length; i++) {
+          var client = clients[i];
+          //If site is opened, focus to the site
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
         }
-      }
 
-      //If site is cannot be opened, open in new window
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    })
+        //If site is cannot be opened, open in new window
+        if (clients.openWindow) {
+          return clients.openWindow("/");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   );
 });
