a9a1c01ff444cb4a6aa951913ac1c0d70bc1ca25
Achyuth KP <kpachyuthz@gmail.com>
2022-06-14 09:27:11 +0530

    updated service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,6 +10,7 @@ var files = [
   "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i", //caching 3rd party content
   "./assets",
   "./manifest.json",
+  "./portfolio.html",
 ];
 
 //Adding `install` event listener
